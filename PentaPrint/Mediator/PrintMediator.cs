6810a727953e0de5b52bc1228c1849d60168b583
welleby <tommywelleby@gmail.com>
2017-04-27 07:37:13 +0200

    Adding PrintMediator

---
+++ PentaPrint/Mediator/PrintMediator.cs
@@ -0,0 +1,14 @@
+﻿using PentaPrint.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PentaPrint.Mediator
+{
+    class PrintMediator
+    {
+        Dictionary<String, IPrint> printables;
+    }
+}
